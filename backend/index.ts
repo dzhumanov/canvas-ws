fdddb1d4fa98294b8a208701647a947e39bf71f4
Temirlan <temadjumanov@gmail.com>
2024-03-11 00:23:26 +0600

    first version of backend logic

---
+++ backend/index.ts
@@ -0,0 +1,53 @@
+import cors from "cors";
+import express from "express";
+import expressWs from "express-ws";
+import { ActiveConnections, IncomingAction } from "./types";
+
+const app = express();
+expressWs(app);
+
+const port = 8000;
+
+app.use(cors());
+
+const router = express.Router();
+
+const activeConnections: ActiveConnections = {};
+router.ws("/canvas", (ws, req) => {
+  const id = crypto.randomUUID();
+  console.log("Client connected id=", id);
+  activeConnections[id] = ws;
+  let canvas: string[] = [];
+
+  ws.send(
+    JSON.stringify({
+      type: "WELCOME",
+      payload: "Hello, you have connected to the canvas!",
+    })
+  );
+
+  ws.on("paint", (paint) => {
+    const parsedMessage = JSON.parse(paint.toString()) as IncomingAction;
+    if (parsedMessage.type === "DRAW") {
+      canvas.push(parsedMessage.payload);
+      Object.values(activeConnections).forEach((connection) => {
+        const outgoingData = {
+          type: "NEW_DATA",
+          payload: canvas,
+        };
+        connection.send(JSON.stringify(outgoingData));
+      });
+    }
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected", id);
+    delete activeConnections[id];
+  });
+});
+
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Server started on ${port} port!`);
+});
